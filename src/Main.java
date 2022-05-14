a112e4d9ed18dcebf77d165d0988ca73eea3780b
glebalef <gleb.alef@gmail.com>
2022-05-14 17:36:22 +0300

    Домашка 9 - готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.sql.SQLOutput;
+
 public class Main {
+
+    // Метод, чтобы сразу печатать навзание книги, автора и год
+    private static void printBookData(Book book) {
+        System.out.println("Название книги: "+book.getBookName());
+        System.out.println("Автор книги: "+book.getAuthor().getName()+" "+book.getAuthor().getSurname());
+        System.out.println("Год издания книги: "+book.getYear());
+        System.out.println("------------------------------------------");
+    }
+    // Книги и авторы для задачи:
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author bAkunin = new Author("Борис", "Акунин");
+        Author sLem = new Author("Станислав", "Лем");
+        Book almazKoles = new Book ("Алмазная колесница", bAkunin, 2002);
+        Book solaris = new Book("Солярис", sLem, 1961);
+
+        printBookData(almazKoles);
+        printBookData(solaris);
+
+        // проверка работы Сеттера
+        almazKoles.setYear(2022);
+        System.out.println("Книга переиздана в году: "+almazKoles.getYear());
+
     }
 }
